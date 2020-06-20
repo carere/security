81e8368cca0f699b3350f542cc2cbf00929ea4bb
Kevin Abatan <abatan.k@gmail.com>
2020-06-20 02:39:41 +0200

    fix class name

--- src/Infrastructure/Laravel/Migrations/2020_06_19_234839_create_addworking_enterprises_has_modules_table.php
+++ src/Infrastructure/Laravel/Migrations/2020_06_19_234839_create_addworking_enterprises_has_modules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAddworkingEnterprisesHasModules extends Migration
+class CreateAddworkingEnterprisesHasModulesTable extends Migration
 {
     /**
      * Run the migrations.
