5079c00a4edce638b485b620cae174431c76d400
Kevin Abatan <abatan.k@gmail.com>
2020-06-20 02:22:19 +0200

    add joint table migration

--- src/Infrastructure/Laravel/Migrations/2020_06_19_234839_create_table_addworking_enterprises_has_modules.php
+++ src/Infrastructure/Laravel/Migrations/2020_06_19_234839_create_table_addworking_enterprises_has_modules.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAddworkingCommonModulesTable extends Migration
+class CreateAddworkingEnterpriseHasModules extends Migration
 {
     /**
      * Run the migrations.
@@ -13,21 +13,23 @@ class CreateAddworkingCommonModulesTable extends Migration
      */
     public function up()
     {
-        Schema::create('addworking_common_modules', function (
+        Schema::create('addworking_enterprises_has_modules', function (
             Blueprint $table
         ) {
-            $table->uuid('id');
-            $table->uuid('parent_id');
-            $table->string('name');
-            $table->string('description');
-            $table->timestamps();
-
-            $table->primary('id');
+            $table->uuid('enterprise_id');
+            $table->uuid('module_id');
 
             $table
-                ->foreign('parent_id')
+                ->foreign('enterprise_id')
+                ->references('id')
+                ->on('addworking_enterprise_members');
+
+            $table
+                ->foreign('module_id')
                 ->references('id')
                 ->on('addworking_common_modules');
+
+            $table->timestamps();
         });
     }
 
@@ -38,6 +40,6 @@ class CreateAddworkingCommonModulesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('addworking_common_modules');
+        Schema::dropIfExists('addworking_enterprises_has_modules');
     }
 }
