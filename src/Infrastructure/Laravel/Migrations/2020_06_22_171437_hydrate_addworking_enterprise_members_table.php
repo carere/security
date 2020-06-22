6df691f627497d4c48007076c06137af062b6f61
Kevin Abatan <abatan.k@gmail.com>
2020-06-22 20:35:07 +0200

    finishing migration for hydrating members

--- src/Infrastructure/Laravel/Migrations/2020_06_22_171437_hydrate_addworking_enterprise_members_table.php
+++ src/Infrastructure/Laravel/Migrations/2020_06_22_171437_hydrate_addworking_enterprise_members_table.php
@@ -14,12 +14,15 @@ class HydrateAddworkingEnterpriseMembersTable extends Migration
     public function up()
     {
         DB::table('addworking_enterprise_enterprises_has_users')
-            ->orderBy('id')
+            ->orderBy('created_at')
             ->each(function (object $row) {
                 DB::table('addworking_enterprise_members')->insert([
                     'id' => Uuid::uuid4(),
                     'user_id' => $row->user_id,
                     'enterprise_id' => $row->enterprise_id,
+                    'created_at' => $row->created_at,
+                    'updated_at' => $row->updated_at,
+                    'deleted_at' => $row->deleted_at,
                 ]);
             });
     }
